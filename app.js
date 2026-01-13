5469601b59d3004074fcd5c0f77bd15acff8f03e
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-13 16:04:44 +1300

    added form, navbar, message and updated indexroutr

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
 const assetsPath = path.join(__dirname, "public");
+
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter );
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
@@ -14,10 +20,3 @@ app.listen(PORT, (error) => {
     console.log(`Mini Message Board - listening on port ${PORT}!`);
 });
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/new", (req, res) => {
-  res.render("index");
-});
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const indexRouter = Router();
+
 const messages = [
   {
     text: "Hi there!",
@@ -13,11 +14,27 @@ const messages = [
   }
 ];
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "new", text: "New Message" },
+];
 
-indexRouter.get("/", (req, res) => res.send("index"));
+indexRouter.get("/", (req, res) => {
+  res.render("index", {title: "Mini Messageboard", messages: messages, links: links});
+});
+
+indexRouter.get("/new", (req, res) => {
+  res.render("form", {title: "New Message", links: links})
+});
+
 indexRouter.get("/:indexId", (req, res) => {
   const { indexId } = req.params;
   res.send(`Index ID: ${indexId}`);
 });
+
+indexRouter.post("/new", (req, res) => {
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+  res.redirect("/");
+})
 
 module.exports = indexRouter;
